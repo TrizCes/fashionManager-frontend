d8a009055394403ad8b3da812211b9ebdb12543e
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2023-11-13 16:36:20 -0300

    Arquitetura base do projeto

---
+++ fashion-manager/src/app/layouts/full/full.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-full',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './full.component.html',
+  styleUrl: './full.component.css'
+})
+export class FullComponent {
+
+}
